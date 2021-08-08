c20410b855beb3675b58c6d31ae61d7578fc0418
Rafli Rai Rizky <rairizky37@gmail.com>
2021-08-08 16:35:38 +0700

    [add] page js for routing

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,15 @@
 	import Home from './pages/Home.svelte';
 	import About from './pages/About.svelte';
 	import Contact from './pages/Contact.svelte';
+
+	import router from 'page';
+
+	let page;
+	router('/', () => (page = Home));
+	router('/about', () => (page = About));
+	router('/contact', () => (page = Contact));
+
+	router.start();
 </script>
 
-<Contact />
+<svelte:component this={page} />
